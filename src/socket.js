dc5ff14895fc6784d643ac1de4ac7068b70c5139
jamal <jjazarin@amazon.com>
2018-04-18 10:50:06 -0700

    Maintain a list of active users.

--- src/socket.js
+++ src/socket.js
@@ -3,10 +3,23 @@ const db = require('./db');
 const onSocketConnect = io => socket => {
 
   // TODO 2.1 Listen for login events (eg "LOGIN") from client and save the user using db.create(username, socket.id)
-  
+  socket.on('LOGIN', ({username}, login) => {
+    if(db.get(username)){
+      if(typeof login === 'function') {
+        login(`${username} is taken.`);
+      } else{
+        logout = db.create(username, socket.id);
+        io.emit('UPDATE_USER_LIST', {users: db.all()});
+      }
+    }
+  });
   // TODO 2.2 Prevent users from using an existing username using the "acknowledgement" from the client
   // TODO 2.3 Emit an update user list event (eg "UPDATE_USER_LIST") to all clients when there is a login event
   // TODO 2.4 Listen for "disconnect" events and remove the socket user from the users object (*hint: db.create(username, socket.id) returns the logout fn)
+  socket.on('disconnect', () => {
+    if(typeof logout === 'function') logout();
+    io.emit('UPDATE_USER_LIST', {users: db.all()});
+  })
   // TODO 2.5 emit "UPDATE_USER_LIST" after user has been "logged out" and is removed from "users" object
 
   // TODO 3.1 Check if a "toUser" is specified and only broadcast to that user
