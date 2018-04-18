dc30eed85decffbb54aac5cd6033cfbf8da45081
jamal <jjazarin@amazon.com>
2018-04-18 10:15:45 -0700

    Modify socket.js to listen for events.

--- src/socket.js
+++ src/socket.js
@@ -3,6 +3,7 @@ const db = require('./db');
 const onSocketConnect = io => socket => {
 
   // TODO 2.1 Listen for login events (eg "LOGIN") from client and save the user using db.create(username, socket.id)
+  
   // TODO 2.2 Prevent users from using an existing username using the "acknowledgement" from the client
   // TODO 2.3 Emit an update user list event (eg "UPDATE_USER_LIST") to all clients when there is a login event
   // TODO 2.4 Listen for "disconnect" events and remove the socket user from the users object (*hint: db.create(username, socket.id) returns the logout fn)
@@ -12,14 +13,18 @@ const onSocketConnect = io => socket => {
   // TODO 3.2 Include information about the "fromUser" so the client can filter draw events from other users and only display events from the selected user
 
   // TODO 1.4 listen for draw action-type events (eg "DRAW_POINTS") from the socket and broadcast them to others sockets.
-
+  socket.on('DRAW_POINTS', ({points, color}) => {
+    socket.broadcast.emit('DRAW_POINTS', {points, color});
+  });
 };
 
 const connect = server => {
   // TODO 1.1 import socket.io
+  const io = require('socket.io')(server);
   // TODO 1.2 attach a socket to the express server by passing the express server instance as an argument when socket.io is invoked
 
   // TODO 1.3 listen for new connections and use the provided "onSocketConnect" function
+  io.on('connect',onSocketConnect(io));
 }
 
 module.exports = connect;
